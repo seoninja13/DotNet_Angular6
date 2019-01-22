11f8500d3669e3529f65340311bd496fdbbd43ca
seoninja13 <dachevivo@gmail.com>
2019-01-22 13:10:01 -0800

    End od section 8: Extending the API

--- DatingApp.API/Dtos/PhotosForDetailedDto.cs
+++ DatingApp.API/Dtos/PhotosForDetailedDto.cs
@@ -7,7 +7,7 @@ namespace DatingApp.API.Dtos
        public int Id { get; set; }
         public string Url { get; set; }
         public string Description { get; set; }
-        public DateTime MyProperty { get; set; }
+        public DateTime DateAdded { get; set; }
         public bool IsMain { get; set; }  
     }
 }
